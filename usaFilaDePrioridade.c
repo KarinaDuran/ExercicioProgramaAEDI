8559ebaf3ee4495f0e8dbcfbb9e562b8f7511dd9
Karina Duran Munhos <63967857+KarinaDuran@users.noreply.github.com>
2020-10-11 17:00:46 -0300

    Update usaFilaDePrioridade.c

--- usaFilaDePrioridade.c
+++ usaFilaDePrioridade.c
@@ -1,5 +1,5 @@
 #include "filaDePrioridade.c"
-
+//Testar filaDePrioridade
 int main() {
   PFILA f = criarFila(10);
   exibirLog(f);
@@ -15,11 +15,10 @@ int main() {
   if(inserirElemento(f, 0, 0)) printf("(4) Funcao retornou true\n");
   else printf("Funcao retornou false (4)\n");
   exibirLog(f);
-  if(inserirElemento(f, 0, 5)) printf("(5) Funcao retornou true\n");
+  if(inserirElemento(f, 5, 5)) printf("(5) Funcao retornou true\n");
   else printf("Funcao retornou false (5)\n");
   exibirLog(f);
 
-/*
   printf("\n\nAUMENTAR PRIORIDADE\n");
   if(aumentarPrioridade(f, 5, 10)) printf("(6) Funcao retornou true\n");
   else printf("Funcao retornou false (6)\n");
@@ -31,14 +30,14 @@ int main() {
   else printf("Funcao retornou false (8)\n");
   exibirLog(f);
   if(aumentarPrioridade(f, 3, 4)) printf("(9) Funcao retornou true\n");
-  else printf("Funcao retornou false (9) - esperado, pois a nova prioridade nao eh maior\n");
+  else printf("Funcao retornou false (9)\n");
   exibirLog(f);
   if(aumentarPrioridade(f, 11, 4)) printf("(10) Funcao retornou true\n");
-  else printf("Funcao retornou false (10) - esperado, elemento com id=4 nao existe\n");
+  else printf("Funcao retornou false (10) \n");
   exibirLog(f);
 
 
-printf("\n\nEXCLUIR ELEMENTO\n");
+  printf("\n\nEXCLUIR ELEMENTO\n");
   PONT prioritario;
   prioritario = removerElemento(f);
   if (prioritario) printf("Prioritario: %i, %f\n", prioritario->id, prioritario->prioridade);
@@ -91,17 +90,16 @@ printf("\n\nEXCLUIR ELEMENTO\n");
   else printf("Funcao retornou false (16)\n");
   exibirLog(f);
   if(reduzirPrioridade(f, 0, 1)) printf("(17) Funcao retornou true\n");
-  else printf("Funcao retornou false (17) - esperado, nova prioridade eh maior\n");
+  else printf("Funcao retornou false (17)\n");
   exibirLog(f);
   if(reduzirPrioridade(f, 3, 2)) printf("(18) Funcao retornou true\n");
   else printf("Funcao retornou false (18)\n");
   exibirLog(f);
   if(reduzirPrioridade(f, 3, 2)) printf("(19) Funcao retornou true\n");
-  else printf("Funcao retornou false (19) - esperado, nova prioridade eh igual\n");
+  else printf("Funcao retornou false (19)\n");
   exibirLog(f);
   if(reduzirPrioridade(f, 4, 1)) printf("(20) Funcao retornou true\n");
-  else printf("Funcao retornou false (20) - esperado, elemento com id=4 nao existe\n");
+  else printf("Funcao retornou false (20)\n");
   exibirLog(f);
-*/
   return 0;
 }
